e45dc2b47a9a2a069b68835ca2c40f34ccfa7f4a
Bhavesh-Tiwari98 <tiwaribhavwsh2211@gmail.com>
2025-09-05 14:17:47 +0530

    Create select query.sql

---
+++ select query.sql
@@ -0,0 +1,21 @@
+select * from product ;
+select * from Category ;
+select * from customer ;
+select * from customer_order ;
+select * from order_details ;
+select * from payment ;
+
+select * from product where id=10;
+
+
+
+
+
+
+
+
+
+
+
+
+
