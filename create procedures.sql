722c0ff646f16e0cced7680ab8d1fa0a5d1ed712
Bhavesh-Tiwari98 <tiwaribhavwsh2211@gmail.com>
2025-09-05 17:51:47 +0530

    Update create procedures.sql

--- create procedures.sql
+++ create procedures.sql
@@ -9,12 +9,37 @@ exec a1;
 create procedure b1
 as
 begin
-
 select category_id ,category_name from category;
 end;
 
 exec b1;
 
+CREATE PROCEDURE c1
+    @CustomerID INT
+AS
+BEGIN
+    SELECT
+        co.order_id,
+        co.order_date,
+        co.total_amount,
+        od.quantity,
+        od.orderprice,
+        p.product_name,
+        p.price AS current_product_price
+    FROM
+        Customer_Order AS co
+    JOIN
+        Order_Details AS od ON co.order_id = od.order_id
+    JOIN
+        Product AS p ON od.product_id = p.product_id
+    WHERE
+        co.customer_id = @CustomerID
+    ORDER BY
+        co.order_date DESC;
+END;
+
+EXEC c1 @CustomerID = 1;
+EXEC c1 @CustomerID = 2;
 
 
 
